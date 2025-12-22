63b21acf3c427bb0603bb2bcaab0bf90bc9efc1e
hamzashadeez <hahmad1178@gmail.com>
2025-12-22 20:42:27 +0100

    app taking shape after 30 mins

---
+++ app/components/Header.tsx
@@ -0,0 +1,16 @@
+"use client"
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='h-17.5 shadow-md bg-white flex items-center justify-between px-6'>
+        <h1>Reels</h1>
+        <div className="flex gap-2 items-center">
+            <button className='bg-white px-4 py-2 rounded-sm cursor-pointer hover:bg-gray-100'>Login</button>
+            <button className='bg-white px-4 py-2 rounded-sm cursor-pointer hover:bg-gray-100'>Sign Up</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ app/components/Reel.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+
+function Reel() {
+  return (
+    <div className="snap-start h-[calc(100vh-70px)]  flex items-center justify-center">
+      <div className="shadow-sm rounded-md overflow-hidden w-4/5 bg-red-400 h-135">
+        Reel
+      </div>
+    </div>
+  );
+}
+
+export default Reel;
