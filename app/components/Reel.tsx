5190b95898dbff70720efe6f47d9e15c2adcb52b
hamzashadeez <hahmad1178@gmail.com>
2025-12-22 20:43:15 +0100

    update

--- app/components/Reel.tsx
+++ app/components/Reel.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function Reel() {
   return (
     <div className="snap-start h-[calc(100vh-70px)]  flex items-center justify-center">
-      <div className="shadow-sm rounded-md overflow-hidden w-4/5 bg-red-400 h-135">
+      <div className="shadow-sm rounded-md overflow-hidden w-4/5 bg-white h-135">
         Reel
       </div>
     </div>
