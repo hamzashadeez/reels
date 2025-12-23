d8b02457da0c11c521f3c8edb34a7b0f6570d520
hamzashadeez <hahmad1178@gmail.com>
2025-12-23 13:35:07 +0100

    update

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,10 +4,10 @@ import React from 'react'
 function Header() {
   return (
     <div className='h-17.5 shadow-md bg-white flex items-center justify-between px-6'>
-        <h1>Reels</h1>
+        <h1 className='text-black'>Reels</h1>
         <div className="flex gap-2 items-center">
-            <button className='bg-white px-4 py-2 rounded-sm cursor-pointer hover:bg-gray-100'>Login</button>
-            <button className='bg-white px-4 py-2 rounded-sm cursor-pointer hover:bg-gray-100'>Sign Up</button>
+            <button className='bg-white text-black px-4 py-2 rounded-sm cursor-pointer hover:bg-gray-100'>Login</button>
+            <button className='bg-white text-black px-4 py-2 rounded-sm cursor-pointer hover:bg-gray-100'>Sign Up</button>
         </div>
     </div>
   )
--- app/components/Reel.tsx
+++ app/components/Reel.tsx
@@ -1,5 +1,8 @@
 "use client";
 import React from "react";
+import { BiChat } from "react-icons/bi";
+import { FaThumbsUp } from "react-icons/fa";
+import { HiHeart } from "react-icons/hi";
 
 function Reel() {
   return (
@@ -8,10 +11,16 @@ function Reel() {
        <video src="/2.mp4" autoPlay  loop className="h-full  w-full"></video>
       </div>
       <div className=' h-full py-10 px-2 flex flex-col justify-end gap-4'>
-        <h1>1</h1>
-        <h1>2</h1>
-        <h1>1</h1>
-        <h1>2</h1>
+       
+        <button className="w-10 h-10 border-none outline-none flex items-center justify-center rounded-full bg-pink-700 hover:bg-pink-400 cursor-pointer">
+          <HiHeart />
+        </button>
+         <button className="w-10 h-10 border-none outline-none flex items-center justify-center rounded-full bg-blue-800 hover:bg-blue-600 cursor-pointer">
+          <FaThumbsUp />
+        </button>
+         <button className="w-10 h-10 border-none outline-none flex items-center justify-center rounded-full bg-orange-600 hover:bg-orange-400 cursor-pointer">
+          <BiChat />
+        </button>
     </div>
     </div>
   );
